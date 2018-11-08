77938f8d7f9b4822752df729e03978105561cde7
Fodor Ciprian <fodorciprian13@gmail.com>
2018-11-08 15:26:05 +0200

    [Job]Added creation

--- src/app/modules/app-jobs/app-jobs.component.ts
+++ src/app/modules/app-jobs/app-jobs.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Subject} from 'rxjs';
+import {ProfileService} from '../../shared/services/profile.service';
 
 @Component({
-  selector: 'app-app-jobs',
-  templateUrl: './app-jobs.component.html',
-  styleUrls: ['./app-jobs.component.scss']
+    selector: 'app-app-jobs',
+    templateUrl: './app-jobs.component.html',
+    styleUrls: ['./app-jobs.component.scss']
 })
-export class AppJobsComponent implements OnInit {
+export class AppJobsComponent implements OnInit, OnDestroy {
+    private navigateToOtherComponent: Subject<any> = new Subject();  //destroy all subscriptions when component is destroyed
 
-  constructor() { }
+    constructor(
+        private router: Router,
+        private activatedRoute: ActivatedRoute) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    onCreateJob() {
+        this.router.navigate(['create'], {relativeTo: this.activatedRoute});
+    }
+
+    ngOnDestroy() {
+        this.navigateToOtherComponent.next();
+        this.navigateToOtherComponent.complete();
+    }
 
 }
--- src/app/shared/config/urls.ts
+++ src/app/shared/config/urls.ts
@@ -1,5 +1,6 @@
 export const urls = {
     loginUserUrl: ['api', 'security', 'login'].join('/'),
     getProfileUrl: ['api', 'user', 'profile'].join('/'),
-    registerUserUrl: ['api', 'register'].join('/')
+    registerUserUrl: ['api', 'register'].join('/'),
+    jobCreateUrl: ['api', 'job'].join('/')
 };
