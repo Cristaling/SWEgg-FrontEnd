520ac98a8edef783b8c213a0c3c4838ad201707e
unknown <bogdan_danut_11@yahoo.com>
2019-01-10 22:42:33 +0200

    fixed remained chunk

--- src/app/modules/user-profile/reviews/reviews.module.ts
+++ src/app/modules/user-profile/reviews/reviews.module.ts
@@ -1,16 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ReviewsComponent} from './components/review/reviews.component';
-import {RatingModule} from 'ngx-rating';
 import {SharedModule} from '../../../shared/shared.module';
 import {ReviewsService} from './services/reviews.service';
 import { RatingComponent } from './components/rating/rating/rating.component';
-import {HttpClientModule} from '@angular/common/http';
-
 @NgModule({
     imports: [
         CommonModule,
-        RatingModule,
         SharedModule
     ],
     declarations: [ReviewsComponent, RatingComponent],
